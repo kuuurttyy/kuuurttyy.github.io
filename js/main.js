f99793082b599c99fcdf369340c4f7c0ace6d5fd
Kurt <kuuurttyy@hotmail.co.uk>
2016-01-12 19:12:52 +0000

    trying to get section scrolling to work, started project css

---
+++ js/main.js
@@ -0,0 +1,19 @@
+//// init scrollMagic controller
+//var controller = new ScrollMagic.Controller();
+//
+//$(document).ready(function () {
+//
+//    var aboutSection = new ScrollMagic.Scene({
+//            triggerHook: "onLeave",
+//            triggerElement: "#about",
+//            duration: "100%"
+//        })
+//        .setPin("#about .section-title")
+//        .addTo(controller);
+//
+//
+//
+//
+//
+//
+//}); //close document.ready
